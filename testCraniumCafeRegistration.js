262e740db4c8ebec57ddd0c3cebb1e12e054a005
Kole Dallam <kolejasonD@gmail.com>
2023-06-20 00:34:03 -0600

    cleaning up and adding comments

--- testCraniumCafeRegistration.js
+++ testCraniumCafeRegistration.js
@@ -1,27 +1,27 @@
-console.log('Starting the test.');
+// Start of test
 
 const { Builder, By, Key, until } = require('selenium-webdriver');
 
 async function testCraniumCafeRegistration() {
-
+    // Enter in the information of who's account you're trying to create
     const testFullName = 'Test Name';
     const testEmail = 'BN@test.com';
     const testPassword = 'Testpassword1';
 
-  console.log('Initializing driver.');
+  // Initializes Driver
   const driver = await new Builder().forBrowser('chrome').build();
   try {
-    console.log('Opening URL.');
+    // Open's URL
     await driver.get('https://my.test.craniumcafe.com/');
 
-    console.log('Searching for school...');
+    // Searches for School
     
       await driver.wait(until.elementLocated(By.name('schoolSearch')), 20000);
       const searchBox = await driver.findElement(By.name('schoolSearch'));
       await searchBox.sendKeys('Cranium Cafe - Test', Key.RETURN);
       await driver.sleep(1500); // Delay of 1.5 Sec 
     
-    console.log('Clicking redirect button...');
+    // Clicks redirect button after search is executed
     
       await driver.wait(until.elementLocated(By.id('integration-redirect-button')), 20000);
       await driver.sleep(1500); 
@@ -31,10 +31,9 @@ async function testCraniumCafeRegistration() {
       const handles = await driver.getAllWindowHandles();
       await driver.switchTo().window(handles[1]);
       await driver.wait(until.urlContains('https://cc.test.craniumcafe.com/login'), 20000);
-      // Line 35 is the switching Iframe that all these buttons are sitting in
       await driver.wait(until.ableToSwitchToFrame(0), 20000);
-      await driver.wait(until.elementLocated(By.xpath('/html/body/div[1]/div[5]/div[2]/button')), 20000);
-      const guestRegButton = await driver.findElement(By.xpath('/html/body/div[1]/div[5]/div[2]/button'));
+      await driver.wait(until.elementLocated(By.id('craniumcafe-button')), 20000);
+      const guestRegButton = await driver.findElement(By.id('craniumcafe-button'));
       await guestRegButton.click();
       await driver.sleep(1500); 
       await driver.wait(until.elementLocated(By.id('register-toggle-button')), 10000);
@@ -42,7 +41,7 @@ async function testCraniumCafeRegistration() {
       await registerToggleButton.click();
       await driver.sleep(1500); 
 
-    console.log('Completing registration form...');
+    // Fills out registration form
     
       await driver.wait(until.elementLocated(By.id('fullname-text')), 10000);
       const fullNameInput = await driver.findElement(By.id('fullname-text'));
@@ -61,31 +60,31 @@ async function testCraniumCafeRegistration() {
       await confirmPasswordInput.sendKeys(testPassword);
       await driver.sleep(1500); 
 
-    console.log('Clicking register button...');
+    // Clicks register button
     
       await driver.wait(until.elementLocated(By.id('register-button')), 10000);
       const registerButton = await driver.findElement(By.id('register-button'));
       await registerButton.click();
       await driver.sleep(1500); 
 
-    console.log('Clicking "Return to sign in" button...');
+    // Clicks return to sign in
     
       await driver.wait(until.elementLocated(By.linkText('Return to sign in')), 10000);
       const returnToSignInButton = await driver.findElement(By.linkText('Return to sign in'));
       await returnToSignInButton.click();
       await driver.sleep(1500); 
 
-    console.log('Clicking "Guest Registration" button again...');
+    // Navigates back to login
     
       await driver.wait(until.urlContains('https://cc.test.craniumcafe.com/login'), 20000);
       await driver.switchTo().defaultContent();
       await driver.wait(until.ableToSwitchToFrame(0), 20000);
-      await driver.wait(until.elementLocated(By.xpath('/html/body/div[1]/div[5]/div[2]/button')), 20000);
-      const secondGuestRegButton = await driver.findElement(By.xpath('/html/body/div[1]/div[5]/div[2]/button'));
+      await driver.wait(until.elementLocated(By.id('craniumcafe-button')), 20000);
+      const secondGuestRegButton = await driver.findElement(By.id('craniumcafe-button'));
       await secondGuestRegButton.click();
       await driver.sleep(1500); 
 
-    console.log('Logging in...');
+    // Logs you in based off what Name, Email and Password you enter
     
       await driver.switchTo().defaultContent();
       await driver.wait(until.ableToSwitchToFrame(0), 20000);
@@ -103,12 +102,12 @@ async function testCraniumCafeRegistration() {
       await driver.sleep(1500); 
 
   } catch (error) {
-    console.log("Caught an error:", error);
+    // Catches errors
   } finally {
-    console.log('Quitting driver.');
+    // Terminates driver
     await driver.quit();
   }
 }
 
-console.log('Calling test function.');
+// Calls test function
 testCraniumCafeRegistration().then(() => console.log('Test function called.'));
